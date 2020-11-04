f17586c014e73109671c716210b6408fe934a281
AliLotfi123 <a.lotfi@outlook.com>
2020-11-04 12:57:00 +0100

    testing with api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,4 @@
-import "./styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  console.log("Hello from _app");
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
